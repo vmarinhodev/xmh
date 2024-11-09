29e0a7021386413900fc412739336164c3ac75c5
Vitor Marinho <vitor@uw-s.nl>
2024-11-09 15:00:46 +0100

    payload.config commit

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -15,6 +15,7 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
+  serverURL: 'http://localhost:3000',
   admin: {
     user: Users.slug,
     importMap: {
